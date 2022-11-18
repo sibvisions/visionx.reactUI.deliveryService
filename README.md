deb542be55c017b6bfa379c398f232e095f4cb54
maximilianweidenauer <66732863+maximilianweidenauer@users.noreply.github.com>
2022-11-18 10:37:04 +0100

    changed pictures, updated readme

--- README.md
+++ README.md
@@ -4,6 +4,30 @@ This repository contains a customized reactUI for the [VisionX](https://visionx.
 
 The application is available in VisionX' marketplace for free. It already contains the customized react UI.
 
-**What is custom?**
+## What is customized?
 
-The application contains one screen. In this screen, we replaced...
+### Custom Components
+The application contains one screen. In this screen, we replaced two number editors (Rating and Minimum Order Value) with our own react components.
+
+For the rating, a rating component was used, which displays the number value as stars. You can set the value by pressing the stars.
+
+For the minimum order value, a slider was used, which can set the minimum order value by sliding the slider.
+
+![custom-components-image](./src/readme-images/custom-components-image.PNG)
+
+The source can be found [here.](https://github.com/sibvisions/visionx.reactUI.deliveryService/blob/main/src/screen-wrappers/ScreenWrapperRestaurants.tsx)
+
+[Click here](https://github.com/sibvisions/reactUI.example/tree/main/src/readme-files/custom-component) for more information about custom-components.
+
+### Custom Screen
+Additionally a custom-screen was added which isn't available in VisionX. 
+
+This custom-screen is a simple react-component which contains a ListBox where you can select different values. Next to it is a galleria which will then display the correct images based on the value.
+
+![custom-screen-image](./src/readme-images/custom-screen-image.PNG)
+
+The source can be found [here.](https://github.com/sibvisions/visionx.reactUI.deliveryService/blob/main/src/CustomRestaurantScreen.tsx)
+
+[Click here](https://github.com/sibvisions/reactUI.example/tree/main/src/readme-files/custom-screen) for more information about custom-screens.
+
+Image source: [https://www.pexels.com](https://www.pexels.com)
--- src/CustomRestaurantScreen.tsx
+++ src/CustomRestaurantScreen.tsx
@@ -5,43 +5,43 @@ import { ListBox } from 'primereact/listbox';
 import { Galleria } from 'primereact/galleria';
 
 import burger from "./stock-photos/stock-burger.jpg";
-import burgerThb from "./stock-photos/stock-burger-thumb.jpg";
+//import burgerThb from "./stock-photos/stock-burger-thumb.jpg";
 import burgerKitchen from "./stock-photos/stock-burger-kitchen.jpg";
-import burgerKitchenThb from "./stock-photos/stock-burger-kitchen-thumb.jpg";
+//import burgerKitchenThb from "./stock-photos/stock-burger-kitchen-thumb.jpg";
 import burgerRestaurant from "./stock-photos/stock-burger-restaurant.jpg";
-import burgerRestaurantThb from "./stock-photos/stock-burger-restaurant-thumb.jpg";
+//import burgerRestaurantThb from "./stock-photos/stock-burger-restaurant-thumb.jpg";
 
 import pizza from "./stock-photos/stock-pizza.jpg";
-import pizzaThb from "./stock-photos/stock-pizza-thumb.jpg";
+//import pizzaThb from "./stock-photos/stock-pizza-thumb.jpg";
 import pizzaKitchen from "./stock-photos/stock-pizza-kitchen.jpg";
-import pizzaKitchenThb from "./stock-photos/stock-pizza-kitchen-thumb.jpg";
+//import pizzaKitchenThb from "./stock-photos/stock-pizza-kitchen-thumb.jpg";
 import pizzaRestaurant from "./stock-photos/stock-pizza-restaurant.jpg";
-import pizzaRestaurantThb from "./stock-photos/stock-pizza-restaurant-thumb.jpg";
+//import pizzaRestaurantThb from "./stock-photos/stock-pizza-restaurant-thumb.jpg";
 
 import steak from "./stock-photos/stock-steak.jpg";
-import steakThb from "./stock-photos/stock-steak-thumb.jpg";
+//import steakThb from "./stock-photos/stock-steak-thumb.jpg";
 import steakKitchen from "./stock-photos/stock-steak-kitchen.jpg";
-import steakKitchenThb from "./stock-photos/stock-steak-kitchen-thumb.jpg";
+//import steakKitchenThb from "./stock-photos/stock-steak-kitchen-thumb.jpg";
 import steakRestaurant from "./stock-photos/stock-steak-restaurant.jpg";
-import steakRestaurantThb from "./stock-photos/stock-steak-restaurant-thumb.jpg";
+//import steakRestaurantThb from "./stock-photos/stock-steak-restaurant-thumb.jpg";
 
 const CustomRestaurantScreen:FC<{}> = () => {
     const burgerImages:any[] = [
-        {id: '1', image: burger, alt: 'burger-image', thumbnail: burgerThb},
-        {id: '2', image: burgerKitchen, alt: 'burger-kitchen-image', thumbnail: burgerKitchenThb},
-        {id: '3', image: burgerRestaurant, alt: 'burger-restaurant-image', thumbnail: burgerRestaurantThb}
+        {id: '1', image: burger, alt: 'burger-image', thumbnail: burger},
+        {id: '2', image: burgerKitchen, alt: 'burger-kitchen-image', thumbnail: burgerKitchen},
+        {id: '3', image: burgerRestaurant, alt: 'burger-restaurant-image', thumbnail: burgerRestaurant}
     ];
 
     const pizzaImages:any[] = [
-        {id: '1', image: pizza, alt: 'pizza-image', thumbnail: pizzaThb},
-        {id: '2', image: pizzaKitchen, alt: 'pizza-kitchen-image', thumbnail: pizzaKitchenThb},
-        {id: '3', image: pizzaRestaurant, alt: 'pizza-restaurant-image', thumbnail: pizzaRestaurantThb}
+        {id: '1', image: pizza, alt: 'pizza-image', thumbnail: pizza},
+        {id: '2', image: pizzaKitchen, alt: 'pizza-kitchen-image', thumbnail: pizzaKitchen},
+        {id: '3', image: pizzaRestaurant, alt: 'pizza-restaurant-image', thumbnail: pizzaRestaurant}
     ];
 
     const steakImages:any[] = [
-        {id: '1', image: steak, alt: 'steak-image', thumbnail: steakThb},
-        {id: '2', image: steakKitchen, alt: 'steak-kitchen-image', thumbnail: steakKitchenThb},
-        {id: '3', image: steakRestaurant, alt: 'steak-restaurant-image', thumbnail: steakRestaurantThb}
+        {id: '1', image: steak, alt: 'steak-image', thumbnail: steak},
+        {id: '2', image: steakKitchen, alt: 'steak-kitchen-image', thumbnail: steakKitchen},
+        {id: '3', image: steakRestaurant, alt: 'steak-restaurant-image', thumbnail: steakRestaurant}
     ];
 
     const [selectedRestaurant, setSelectedRestaurant] = useState<"Burger"|"Pizza"|"Steak">("Burger");
@@ -69,11 +69,11 @@ const CustomRestaurantScreen:FC<{}> = () => {
     }, [selectedRestaurant])
 
     const itemTemplate = (item:any) => {
-        return <img src={item.image} alt={item.alt} style={{ width: '100%' }} />
+        return <img src={item.image} alt={item.alt} style={{ width: '100%', maxHeight: "700px" }} />
     }
 
     const thumbnailTemplate = (item:any) => {
-        return <img src={item.thumbnail} alt={item.alt} />
+        return <img src={item.thumbnail} style={{ maxWidth: "80px", maxHeight: "60px" }} alt={item.alt} />
     }
 
     return (
@@ -84,7 +84,7 @@ const CustomRestaurantScreen:FC<{}> = () => {
                 onChange={(e) => setSelectedRestaurant(e.value)} 
                 optionLabel="label" />
             <Galleria value={selectedImages} numVisible={3} item={itemTemplate} thumbnail={thumbnailTemplate} style={{ maxWidth: "1200px", margin: "auto" }} /> 
-            Stock Images
+            source: https://www.pexels.com/ 
         </div>
         
     )
